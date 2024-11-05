d72297f52640940908ef846e8d289701c0db3adc
Mikael Anttila <mikael.antt@gmail.com>
2024-11-06 01:18:37 +0200

    Todo part 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  const addTask = () => {
+    setTasks([...tasks, task])
+    setTask("")
+  }
+
+  const deleteTask = (delItem) => {
+    const removeFilter = tasks.filter((item) => item !== delItem)
+    setTasks(removeFilter)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Todos</h3>
+      <form>
+        <input placeholder="Add new task" 
+        value={task}
+        onChange={e => setTask(e.target.value)}
+        onKeyDown={e => {
+          if (e.key === "Enter") {
+            e.preventDefault()
+            addTask()
+          }
+        }}        
+        />
+      </form>
+      <ul>
+        {
+          tasks.map(item => (
+            <li>{item}
+            <button className="deletebutton" onClick={() => deleteTask(item)}>Delete</button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
