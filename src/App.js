2bf59326ddb2e957c4f8477e2d9e3b7ce51ae29e
Mikael Anttila <mikael.antt@gmail.com>
2024-11-07 00:42:22 +0200

    todo part 4-5 done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import './App.css';
-import {useState} from "react";
+import './App.css'
+import {useEffect, useState} from "react"
+import axios from "axios"
+import Row from './components/Row'
+
+const url = "http://localhost:3001"
 
 function App() {
   const [task, setTask] = useState("")
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setTasks(response.data)
+      }).catch(error => {
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
+  }, [])
+
   const addTask = () => {
-    setTasks([...tasks, task])
-    setTask("")
+    axios.post(url + "/create",{
+      description: task
+    })
+    .then(response =>{
+      setTasks([...tasks, {id: response.data.id, description: task}])
+      setTask("")
+    }).catch(error => {
+      alert(error.response.data.error ? error.response.data.error : error)
+    })
   }
 
-  const deleteTask = (delItem) => {
-    const removeFilter = tasks.filter((item) => item !== delItem)
-    setTasks(removeFilter)
+  const deleteTask = (id) => {
+    axios.delete(url+ "/delete/" + id)
+      .then(response => {
+        const removeFilter = tasks.filter((item) => item.id !== id)
+        setTasks(removeFilter)
+      }).catch(error => {
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
   }
 
   return (
@@ -33,9 +58,7 @@ function App() {
       <ul>
         {
           tasks.map(item => (
-            <li>{item}
-            <button className="deletebutton" onClick={() => deleteTask(item)}>Delete</button>
-            </li>
+            <Row key={item.id} item={item} deleteTask={deleteTask}/>
           ))
         }
       </ul>
